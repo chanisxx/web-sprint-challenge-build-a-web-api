cc7d6c5c9253d7213f8710b7fdb4cbc7cac436be
Chanis Torres <chanis_torres@yahoo.com>
2021-01-23 16:47:34 -0500

    server up, Router and Model

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,92 @@
 // Write your "actions" router here!
+const express = require('express');
+const Actions = require('./actions-model');
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+    console.log('In the actions router');
+    next();
+});
+
+router.get('/', (req, res) => {
+    Actions.get()
+    .then(r => {
+      res.status(200).json(r);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the actions',
+      });
+    });
+});
+
+// add a action
+router.post('/:id', validateAction, validateActionId, (req, res) => {
+    Actions.insert(req.action)
+    .then(action => {
+      res.status(201).json(action);
+    })
+    .catch(err => {
+      res.status(500).json({message: "Error adding an action", err});
+    })
+})
+
+//modify an action
+router.put('/:id', validateAction,validateActionId,(req, res) => {
+    Actions.update(req.params.id, req.body)
+    .then(actions => {
+      res.status(200).json(`Action ${actions.notes} updated`);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({message: 'There was an error updating action'})
+    })
+})
+
+//delete action
+router.delete('/:id', validateActionId, (req, res) => {
+    Actions.remove(req.params.id)
+    .then(count => {
+      res.status(200).json({ message: `${count} account deleted`})
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: 'Error removing the action' });
+    });
+})
+
+function validateAction(req, res, next) {
+    const input = ["notes", "description"];
+    const request = req.body;
+  if(request) {
+    if(input.every(key => request.hasOwnProperty(key))) {
+        const action = {...req.body, project_id: req.params.id};
+        req.action = action;
+      next();
+    } else {
+      next({code: 400, message: "Missing required name or description field"});
+    }
+  } else {
+    next({code: 400, message: "Missing user data", user: req.body});
+  }
+}
+
+function validateActionId(req, res, next) {
+    const { id } = req.params;
+    Actions.get(id)
+    .then(action => {
+      if(action) {
+        next();
+      } else {
+        next({code: 400, message: "Invalid action ID"});
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      next({code: 500, message: "Failed to process request", err});
+    })
+  }
+
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,118 @@
 // Write your "projects" router here!
+const express = require('express');
+const Projects = require('./projects-model');
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+    console.log('In the project router');
+    next();
+});
+
+// get all projects
+router.get('/', (req, res) => {
+    Projects.get()
+    .then(r => {
+      res.status(200).json(r);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the projects',
+      });
+    });
+});
+
+// add a project
+router.post('/', validateProject, (req, res) => {
+    Projects.insert(req.currentProject)
+    .then(project => {
+      res.status(201).json(project);
+    })
+    .catch(err => {
+      res.status(500).json({message: "Error adding a project", err});
+    })
+})
+
+//modify a project
+router.put('/:id', validateProjectId, validateProject, (req, res) => {
+    Projects.update(req.params.id, req.body)
+    .then(project => {
+      res.status(200).json(`Project ${project.name} updated`);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({message: 'There was an error updating project'})
+    })
+})
+
+//delete id
+router.delete('/:id', validateProjectId, (req, res) => {
+    Projects.remove(req.params.id)
+    .then(count => {
+      res.status(200).json({ message: `${count} account deleted: ${req.project.name}`})
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: 'Error removing the project' });
+    });
+})
+
+// get all actions
+router.get('/:id', validateProjectId, (req, res) => {
+    Projects.getProjectActions(req.params.id)
+    .then(actions => {
+      res.status(200).json(actions)
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: 'Error retrieving the actions' });
+    });
+})
+
+function validateProjectId(req, res, next) {
+    const { id } = req.params;
+    Projects.get(id)
+    .then(project => {
+      if(project) {
+        req.project = project;
+        next();
+      } else {
+        next({code: 400, message: "Invalid project ID"});
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      next({code: 500, message: "Failed to process request", err});
+    })
+  }
+  
+  function validateProject(req, res, next) {
+      const input = ["name", "description"];
+      const request = req.body;
+    if(request) {
+      if(input.every(key => request.hasOwnProperty(key))) {
+        req.currentProject = request;
+        next();
+      } else {
+        next({code: 400, message: "Missing required name or description field"});
+      }
+    } else {
+      next({code: 400, message: "Missing user data", user: req.body});
+    }
+  }
+  
+//   function validateAction(req, res, next) {
+//     // if(req.body && Object.keys(req.body) > 0) {
+//       if(req.body.text) {
+//         next();
+//       } else {
+//         next({code: 404, message: "Please include text"});
+//       }
+//     // } else {
+//       // next({code: 404, message: "Please include a request body"});
+//       // res.status(500).json({message: 'please include a request body'})
+//     // }
+//   };
+
+module.exports = router;
